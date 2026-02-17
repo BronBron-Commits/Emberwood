8ae6701ea017150b74fa7b2609f92e901103de0c
BronBron <brmanley@outlook.com>
2026-02-16 23:50:09 -0500

    Build fixes: link all required SDL2_ttf, FreeType, HarfBuzz, and system libraries for MinGW/MSYS2

---
+++ build/CMakeFiles/Emberwood.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Emberwood.dir/src/engine/Renderer.cpp.obj"
+  "CMakeFiles/Emberwood.dir/src/engine/Renderer.cpp.obj.d"
+  "CMakeFiles/Emberwood.dir/src/engine/TextureManager.cpp.obj"
+  "CMakeFiles/Emberwood.dir/src/engine/TextureManager.cpp.obj.d"
+  "CMakeFiles/Emberwood.dir/src/entities/Tree.cpp.obj"
+  "CMakeFiles/Emberwood.dir/src/entities/Tree.cpp.obj.d"
+  "CMakeFiles/Emberwood.dir/src/main.cpp.obj"
+  "CMakeFiles/Emberwood.dir/src/main.cpp.obj.d"
+  "Emberwood.exe"
+  "Emberwood.exe.manifest"
+  "Emberwood.pdb"
+  "libEmberwood.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Emberwood.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
